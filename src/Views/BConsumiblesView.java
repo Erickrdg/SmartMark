852109acf96e5ed6c0df9f2ea08d326c9e28e72d
Erickrdg <erickfirex@gmail.com>
2021-01-06 04:33:10 -0600

    Creacion del primer MenuItem ConsumibleBodega

--- src/Views/BConsumiblesView.java
+++ src/Views/BConsumiblesView.java
@@ -27,15 +27,44 @@ public class BConsumiblesView extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        crearBoton = new javax.swing.JButton();
+        editarBton = new javax.swing.JButton();
+        eliminarBtton = new javax.swing.JButton();
+        recargarBtton = new javax.swing.JButton();
+
+        crearBoton.setText("Crear");
+
+        editarBton.setText("Editar");
+
+        eliminarBtton.setText("Eliminar");
+
+        recargarBtton.setText("Recargar");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(crearBoton)
+                .addGap(18, 18, 18)
+                .addComponent(editarBton)
+                .addGap(18, 18, 18)
+                .addComponent(eliminarBtton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(recargarBtton)
+                .addContainerGap(47, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(202, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(crearBoton)
+                    .addComponent(editarBton)
+                    .addComponent(eliminarBtton)
+                    .addComponent(recargarBtton))
+                .addGap(50, 50, 50))
         );
 
         pack();
@@ -43,5 +72,9 @@ public class BConsumiblesView extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton crearBoton;
+    private javax.swing.JButton editarBton;
+    private javax.swing.JButton eliminarBtton;
+    private javax.swing.JButton recargarBtton;
     // End of variables declaration//GEN-END:variables
 }
